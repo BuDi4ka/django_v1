1b621e8df5b7d108ba8df1805d2802b11cf56e3a
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-04-22 11:44:42 +0100

    challenge completed

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -22,10 +22,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
